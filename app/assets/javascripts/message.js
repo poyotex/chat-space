462c4f9d656ea58cdb3b1e79aa63f093b98d139f
poyotex <tru1121@gmail.com>
2020-03-23 14:51:40 +0900

    overflow: scroll; プロパティ追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -79,7 +79,7 @@ $(function(){
           insertHTML += buildHTML(message)
         });
         $('.messages').append(insertHTML)
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
       }
     })
     .fail(function() {
